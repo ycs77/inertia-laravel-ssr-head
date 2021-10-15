be4b7816de02bff7af8f0077b792e655ba646ed6
Lucas Yang <yangchenshin77@gmail.com>
2021-10-16 01:14:50 +0800

    Add type file in client

---
+++ client/index.d.ts
@@ -0,0 +1,9 @@
+export declare function updateTitle(): void
+
+export declare const InertiaTitleVue2 = {
+  install(Vue: any, options: any): void
+}
+
+export declare const InertiaTitleVue3 = {
+  install(app: any, options: any): void
+}
