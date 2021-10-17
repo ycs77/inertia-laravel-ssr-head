4583ce228766aa52b82f0488544df0c4cdb9d93a
Lucas Yang <yangchenshin77@gmail.com>
2021-10-18 03:02:13 +0800

    Tweak

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Testing\TestResponse;
 use Inertia\Response;
-use Inertia\SSRHead\HTML\Element;
 use Inertia\SSRHead\Tests\TestCase;
-use PHPUnit\Framework\Assert;
 
 uses(TestCase::class)->in(__DIR__);
 
