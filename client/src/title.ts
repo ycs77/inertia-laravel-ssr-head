18394fd426e205590b9d15564af778ed12e72f02
Lucas Yang <yangchenshin77@gmail.com>
2021-10-27 03:19:51 +0800

    Simplify useInertiaTitle()

--- client/src/title.ts
+++ client/src/title.ts
@@ -1,16 +1,12 @@
 import { Inertia } from '@inertiajs/inertia'
 
 export function useInertiaTitle(): void {
-  const originalTitle = document.title
-
   function setTitle(value: string) {
-    if (document.title !== value) {
-      setTimeout(() => document.title = value, 1)
-    }
+    setTimeout(() => document.title = value, 1)
   }
 
   Inertia.on('navigate', event => {
     const title = event.detail.page.props.title as string
-    setTitle(title || originalTitle)
+    setTitle(title)
   })
 }
