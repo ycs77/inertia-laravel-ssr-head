25b420ccd14b76c071a1dba724654c6b6ea9de8d
Lucas Yang <yangchenshin77@gmail.com>
2023-07-15 23:07:34 +0800

    Tweak test stub

--- tests/stubs/views/app.blade.php
+++ tests/stubs/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="robots" content="noindex, nofollow">
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @inertiaHead
 </head>
 
